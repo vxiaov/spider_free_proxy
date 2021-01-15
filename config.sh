5cc3f5a8b27b64d99115ab3538d39b94c51be9a3
Awkee <next4nextjob@gmail.com>
2021-01-15 23:19:10 +0800

    update config.sh

--- config.sh
+++ config.sh
@@ -10,40 +10,40 @@
 init_socks5()
 {
     conf_file="./conf/default_ssr.conf"
-	python3 ./bin/proxy_uri_util.py  > ${conf_file}
+    python3 ./bin/proxy_uri_util.py  > ${conf_file}
     ssr-client -d -c ${conf_file}
 }
 
 
 
 config_python_venv(){
-	venv_name="spider"
-	install_path="/opt/anaconda3"
+    venv_name="spider"
+    install_path="/opt/anaconda3"
 
-	. ${install_path}/etc/profile.d/conda.sh
-	
+    . ${install_path}/etc/profile.d/conda.sh
+
     conda env list| grep ${venv_name} >/dev/null
     if [ "$?" = "0" ] ; then
         echo "env [$venv_name] is already created!"
     else
-	    conda create -yn $venv_name python=3.8
-	    conda init bash
+        conda create -yn $venv_name python=3.8
+        conda init bash
     fi
 
-	conda activate $venv_name
-	pip install -r requirements.txt
-	
-	# 设置工作目录
-    wkdir=`pwd`
-	sed -i "s/^workdir=.*/workdir=${wkdir}/" start.sh
-	sed -i "s/^venv_name=.*/venv_name=${venv_name}/" start.sh
-	sed -i "s/1084/1080/" ./conf/config.ini   # 修改默认代理为临时的1080端口代理
+    conda activate $venv_name
+    pip install -r requirements.txt
 
-	# 添加调度任务
-	echo "----------------------------------------------------------"
-	echo "You may need to add this job to crontab:"
-	echo "*/5 * * * * sh `pwd`/start.sh 'cron' >/dev/null 2>&1"
-	echo "----------------------------------------------------------"
+    # 设置工作目录
+    wkdir=`pwd`
+    sed -i "s/^workdir=.*/workdir=${wkdir}/g" start.sh
+    sed -i "s/^venv_name=.*/venv_name=${venv_name}/g" start.sh
+    sed -i "s/1084/1080/g" ./conf/config.ini   # 修改默认代理为临时的1080端口代理
+
+    # 添加调度任务
+    echo "----------------------------------------------------------"
+    echo "You may need to add this job to crontab:"
+    echo "*/5 * * * * sh `pwd`/start.sh 'cron' >/dev/null 2>&1"
+    echo "----------------------------------------------------------"
 
 }
 
@@ -51,9 +51,9 @@ config_python_venv(){
 # 初始化创建目录#
 for tmpdir in "running" "chrome_profile" "log"
 do
-	if [ ! -r $tmpdir ] ; then
-		mkdir $tmpdir
-	fi
+    if [ ! -r $tmpdir ] ; then
+        mkdir $tmpdir
+    fi
 done
 
 config_python_venv
@@ -65,7 +65,7 @@ curl -x socks5://127.0.0.1:1080 https://httpbin.org/ip
 if [ "$?" = "0" ] ; then
     ./start.sh -p all   # 第一次爬取代理
     ./start.sh -c all   # 第一次验证有效并启动有效代理(有效代理启动后，就可以替换回1084端口了)
-	sed -i "s/1080/1084/" ./conf/config.ini   # 修改代理端口为haproxy代理端口
+    sed -i "s/1080/1084/" ./conf/config.ini   # 修改代理端口为haproxy代理端口
 else
     echo "临时代理1080端口不可用，请更换一个可用代理再进行手工启动"
 fi
