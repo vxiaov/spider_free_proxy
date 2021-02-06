15135e7348fecb8518f9cc9aacdb531a63f4750e
Awkee <next4nextjob@gmail.com>
2021-02-06 18:22:50 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -78,14 +78,20 @@ compile_simple_obfs(){
 ## 1. 安装客户端命令
 install_ss(){
     sudo ${pac_cmd_install} shadowsocks-libev
-    [[ "$?" = "0" ]] || echo "安装 shadowsocks-libev 失败！请手动安装。"
-    which ss-local >/dev/null  && ( echo "测试结果：找不到 ss-local 命令， 安装 ss-local 失败！" ; return 1 ; ) 
-    echo "测试结果： ss-local 命令可用，安装 shadowsocks-libev 成功。"
+    if which ss-local >/dev/null
+    then
+        echo "测试结果： ss-local 命令可用，安装 shadowsocks-libev 成功。"
+    else
+        echo "测试结果：找不到 ss-local 命令， 安装 ss-local 失败！" 
+    fi
 
     sudo ${pac_cmd_install} simple-obfs
-    [[ "$?" = "0" ]] || ( echo "安装 simple-obfs 失败！" &&  compile_simple_obfs ) 
-    which obfs-local >/dev/null  && ( echo "测试结果：找不到 obfs-local 命令， 安装 obfs-local 失败！" ; return 1 ; )
-    echo "测试结果： obfs-local 命令可用，安装 simple-obfs 成功。"
+    [[ "$?" = "0" ]] || compile_simple_obfs
+    if which obfs-local >/dev/null ; then
+        echo "测试结果： obfs-local 命令可用，安装 simple-obfs 成功。"
+    else
+        echo "测试结果：找不到 obfs-local 命令， 安装 obfs-local 失败！"
+    fi
 }
 
 compile_ssr_native(){
@@ -109,7 +115,7 @@ compile_ssr_native(){
 }
 
 install_ssr(){
-    which ssr-client >/dev/null && ( echo "ssr-client 已经安装成功了！"; return 0; )
+    which ssr-client >/dev/null && echo "ssr-client 已经安装成功了！" && return 0 
     
     if [ "$os_type" = "raspbian" ] ; then
         compile_ssr_native
@@ -118,12 +124,15 @@ install_ssr(){
         unzip -o -d ${tmpdir} ssr-native-linux-x64.zip
         sudo cp -pf ${tmpdir}/ssr-client /usr/bin/
     fi
-    which ssr-client >/dev/null || ( echo "验证结果：安装失败！ 找不到 ssr-client 命令!" ;  return 1 ; )
-    echo "验证结果： 安装成功！ 找到了 `which ssr-client` 命令！"
+    if which ssr-client >/dev/null ; then
+        echo "验证结果： 安装成功！ 找到了 `which ssr-client` 命令！"
+    else
+        echo "验证结果：安装失败！ 找不到 ssr-client 命令!"
+    fi
 }
 
 install_v2ray(){
-    which v2ray >/dev/null && ( echo "v2ray 已经安装成功了!" ; return 0; )
+    which v2ray >/dev/null && echo "v2ray 已经安装成功了!" && return 0
 
     if [ "$os_type" = "raspbian" ] ; then
         if [ ! -f v2ray-linux-arm32-v7a.zip ] ; then
@@ -144,12 +153,6 @@ install_haproxy(){
 }
 
 config_haproxy(){
-    # Config DNS nameserver
-    if ! grep -q "8.8.8.8" /etc/resolv.conf; then
-        sudo cp -p /etc/resolv.conf /etc/resolv.conf.bak
-        sudo echo "nameserver 8.8.8.8" > /etc/resolv.conf
-        sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-    fi
 
     dest_file="/etc/haproxy/haproxy.cfg"
     if [ -f $dest_file ]; then
@@ -157,14 +160,79 @@ config_haproxy(){
     fi
 
     # generate socks5 server list info
-    server_list_info=`awk 'BEGIN{for(i=0;i<=150;i++)printf("\tserver  socks5_%03d 127.0.0.1:%d check\n", i, 20000+i); }'`
+    server_list_info=`awk 'BEGIN{for(i=0;i<=200;i++)printf("\tserver  socks5_%03d 127.0.0.1:%d check\n", i, 20000+i); }'`
 
-    haproxy_cfg="conf/haproxy.cfg.template"
-    if [ ! -f "$haproxy_cfg" ] ; then
-        echo "配置文件模板 [$haproxy_cfg] 找不到！"
-        return 1
-    fi
-    sudo sed -np "s#$server_list_info#{server_list_info}#g" ${haproxy_cfg} > /etc/haproxy/haproxy.cfg
+    cat <<END | sudo tee $dest_file
+global
+    log /dev/log daemon
+    maxconn 32768
+    chroot /var/lib/haproxy
+    user haproxy
+    group haproxy
+    daemon
+    tune.bufsize 32768
+    tune.ssl.default-dh-param 2048
+    ssl-default-bind-ciphers ALL:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK:!RC4:!ADH:!LOW@STRENGTH
+
+    stats socket /var/lib/haproxy/stats user haproxy group haproxy mode 0640 level operator
+    stats socket ipv4@127.0.0.1:9999 level admin
+    stats socket /var/lib/haproxy/stats mode 0640 level admin
+    stats timeout 2m
+
+defaults
+    mode                    tcp
+    log                     global
+    option                  httplog
+    option                  dontlognull
+    option                  http-server-close
+    option                  redispatch
+    retries                 3
+    timeout http-request    10s
+    timeout queue           1m
+    timeout connect         10s
+    timeout client          1m
+    timeout server          1m
+    timeout http-keep-alive 10s
+    timeout check           10s
+    maxconn                 3000
+
+# HAProxy 前端服务
+frontend socks5_frontend
+    bind    0.0.0.0:1084
+    mode    tcp
+    option tcplog
+    timeout client  30s
+    default_backend socks5_backend
+
+# HAProxy 后端服务节点的负载均衡配置
+backend socks5_backend
+    mode    tcp
+    option log-health-checks
+    balance leastconn
+    timeout server  30s
+    timeout connect 10s
+${server_list_info}
+
+# HAProxy web ui: http://localhost:19999/haproxy?stats
+listen stats
+    bind 0.0.0.0:19999
+    mode http
+    log global
+
+    maxconn 10
+    timeout client 100s
+    timeout server 100s
+    timeout connect 100s
+    timeout queue 100s
+
+    stats enable
+    stats uri /haproxy?stats
+    stats hide-version
+    stats realm HAProxy\ Statistics
+    stats admin if TRUE
+    stats show-node
+END
+
 }
 
 # start haproxy
